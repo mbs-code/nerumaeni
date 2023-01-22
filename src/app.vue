adaf236a068915d2f35f6f71a59e2217c8cfb7f8
mbs-code <mbscodec@gmail.com>
2023-01-22 21:32:33 +0900

    add: typeorm を搭載

--- src/app.vue
+++ src/app.vue
@@ -10,6 +10,8 @@
           </template>
         </van-cell>
 
+        <div>{{ keepArticle }}</div>
+
         <NuxtPage />
       </div>
 
@@ -26,116 +28,20 @@
 </template>
 
 <script setup lang='ts'>
-import { useSQLite } from 'vue-sqlite-hook'
+import { Article } from '~~/src/databases/entity/article'
 
-const app = getCurrentInstance()
+const keepArticle = ref<Article>()
 
 onMounted(async () => {
-  console.log(' in App on Mounted')
-  if (app != null) {
-    app.appContext.config.globalProperties.$sqlite = useSQLite()
-    const sqlite = app.appContext.config.globalProperties.$sqlite
-    try {
-      let res = await sqlite.echo('Hello from echo')
-      console.log(res)
-      if (res.value !== 'Hello from echo') {
-        console.log('> Echo not returning "Hello from echo"\n')
-        return
-      }
+  const article = new Article()
+  article.date = String(new Date())
+  article.rate = 5
+  article.text = 'asdasd'
+  await article.save()
 
-      console.log('> Echo successful\n')
-      // create a connection for NoEncryption
-      const db = await sqlite.createConnection('NoEncryption')
-      console.log('> createConnection \'NoEncryption\' successful\n')
-      // open NoEncryption database
-      await db.open()
-      console.log('> open \'NoEncryption\' successful\n')
-
-      const createTablesNoEncryption = `
-            CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY NOT NULL,
-            email TEXT UNIQUE NOT NULL,
-            name TEXT,
-            company TEXT,
-            size FLOAT,
-            age INTEGER,
-            last_modified INTEGER DEFAULT (strftime('%s', 'now'))
-            );
-            CREATE TABLE IF NOT EXISTS messages (
-            id INTEGER PRIMARY KEY NOT NULL,
-            userid INTEGER,
-            title TEXT NOT NULL,
-            body TEXT NOT NULL,
-            last_modified INTEGER DEFAULT (strftime('%s', 'now')),
-            FOREIGN KEY (userid) REFERENCES users(id) ON DELETE SET DEFAULT
-            );
-            CREATE INDEX IF NOT EXISTS users_index_name ON users (name);
-            CREATE INDEX IF NOT EXISTS users_index_last_modified ON users (last_modified);
-            CREATE INDEX IF NOT EXISTS messages_index_last_modified ON messages (last_modified);
-            CREATE TRIGGER IF NOT EXISTS users_trigger_last_modified
-            AFTER UPDATE ON users
-            FOR EACH ROW WHEN NEW.last_modified <= OLD.last_modified
-            BEGIN
-                UPDATE users SET last_modified= (strftime('%s', 'now')) WHERE id=OLD.id;
-            END;
-            CREATE TRIGGER IF NOT EXISTS messages_trigger_last_modified AFTER UPDATE ON messages
-            FOR EACH ROW WHEN NEW.last_modified <= OLD.last_modified
-            BEGIN
-                UPDATE messages SET last_modified= (strftime('%s', 'now')) WHERE id=OLD.id;
-            END;
-            PRAGMA user_version = 1;
-          `
-      const importTwoUsers = `
-            DELETE FROM users;
-            INSERT INTO users (name,email,age) VALUES ("Whiteley","Whiteley.com",30);
-            INSERT INTO users (name,email,age) VALUES ("Jones","Jones.com",44);
-          `
-      const importThreeMessages = `
-            DELETE FROM messages;
-            INSERT INTO messages (userid,title,body) VALUES (1,"test post 1","content test post 1");
-            INSERT INTO messages (userid,title,body) VALUES (2,"test post 2","content test post 2");
-            INSERT INTO messages (userid,title,body) VALUES (1,"test post 3","content test post 3");
-          `
-      const dropTablesTablesNoEncryption = `
-            PRAGMA foreign_keys = OFF;
-            DROP TABLE IF EXISTS users;
-            DROP TABLE IF EXISTS messages;
-            PRAGMA foreign_keys = ON;
-          `
-      // Drop tables if exists
-      res = await db.execute(dropTablesTablesNoEncryption, false)
-      console.log(`drop tables res: ${JSON.stringify(res)}`)
-      console.log(`drop tables res: ${JSON.stringify(res)}\n`)
-      if (res.changes.changes < 0) {
-        console.log(' Execute1 failed\n')
-        return
-      }
-      // Create tables
-      res = await db.execute(createTablesNoEncryption)
-      if (res.changes.changes !== 0 && res.changes.changes !== 1) {
-        console.log(' Execute2 failed\n')
-        return
-      }
-      // Insert two users with execute method
-      res = await db.execute(importTwoUsers)
-      if (res.changes.changes !== 2) {
-        console.log(' Execute3 failed\n')
-        return
-      }
-
-      res = await db.execute(importThreeMessages)
-      if (res.changes.changes !== 3) {
-        console.log(' Execute4 failed\n')
-        return
-      }
-
-      // Close Connection NoEncryption
-      await sqlite.closeConnection('NoEncryption')
-      console.log('* Ending test successfully*\n')
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  // eslint-disable-next-line no-console
+  console.log(article)
+  keepArticle.value = article
 })
 
 const route = useRoute()
--- src/plugins/sqlite.ts
+++ src/plugins/sqlite.ts
@@ -1,52 +1,44 @@
 import { CapacitorSQLite, SQLiteConnection } from '@capacitor-community/sqlite'
 import { Capacitor } from '@capacitor/core'
 import { JeepSqlite } from 'jeep-sqlite/dist/components/jeep-sqlite'
+import { InjectionKey } from 'nuxt/dist/app/compat/capi'
+import { initDataSource } from '~~/src/databases/initDataSource'
 
 customElements.define('jeep-sqlite', JeepSqlite)
 
-const database = 'storage'
-const readonly = false
+export const dataSourceKey: InjectionKey<ReturnType<typeof initDataSource>> = Symbol('data-source')
 
 // TODO: public/assets/sql-wasm を消す
 export default defineNuxtPlugin(async (nuxtApp) => {
-  const platform = Capacitor.getPlatform()
-  const sqlite = new SQLiteConnection(CapacitorSQLite)
-
   try {
+    const platform = Capacitor.getPlatform()
+    const sqlite = new SQLiteConnection(CapacitorSQLite)
+
+    // web のときに jeep indexedDB を立ち上げる
     if (platform === 'web') {
-      // Create the 'jeep-sqlite' Stencil component
       const jeepSqlite = document.createElement('jeep-sqlite')
       document.body.appendChild(jeepSqlite)
+
       await customElements.whenDefined('jeep-sqlite')
-      // Initialize the Web store
       await sqlite.initWebStore()
     }
-    // here you can initialize some database schema if required
 
-    // example: database creation with standard SQLite statements
-    const ret = await sqlite.checkConnectionsConsistency()
-    const isConn = (await sqlite.isConnection(database, readonly)).result
-    let db = null
-    if (ret.result && isConn) {
-      db = await sqlite.retrieveConnection(database, readonly)
-    } else {
-      db = await sqlite.createConnection(database, false, 'no-encryption', 1, readonly)
-    }
-    await db.open()
-    const query = `
-      CREATE TABLE IF NOT EXISTS test (
-      id INTEGER PRIMARY KEY NOT NULL,
-      name TEXT NOT NULL
-      );
-    `
-    const res = await db.execute(query)
-    if (res.changes && res.changes.changes && res.changes.changes < 0) {
-      throw new Error('Error: execute failed')
+    // 接続テスト
+    const echoRes = await sqlite.echo('Hello from echo')
+    if (echoRes.value !== 'Hello from echo') {
+      throw new Error('Echo failed.')
     }
 
-    await sqlite.closeConnection(database, readonly)
+    // 接続
+    const dataSource = initDataSource(sqlite)
+    await dataSource.initialize()
+
+    await dataSource.runMigrations()
+
+    // アプリに公開
+    nuxtApp.provide('db', dataSource)
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.log(`Error: ${err}`)
-    throw new Error(`Error: ${err}`)
   }
 })
